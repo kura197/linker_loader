f78fb6202fdb716babdf4fb06495b7fb133dc58a
Ryosuke Kuramochi <kura19971106@gmail.com>
2023-01-22 13:40:13 +0900

    add loader impl

--- loader/loader.cpp
+++ loader/loader.cpp
@@ -7,6 +7,9 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <errno.h>
+
+const uint64_t PAGE_SIZE = 4096;
 
 #define IS_ELF(ehdr) (                   \
     ((ehdr).e_ident[EI_MAG0] == ELFMAG0) && \
@@ -14,101 +17,82 @@
     ((ehdr).e_ident[EI_MAG2] == ELFMAG2) && \
     ((ehdr).e_ident[EI_MAG3] == ELFMAG3))
 
-void elfdump(char* head) {
-    Elf64_Ehdr *ehdr = (Elf64_Ehdr*)head;
-    if (!IS_ELF(*ehdr)) {
-        fprintf(stderr, "This is not ELF file.");
-        return;
-    }
-
-    Elf64_Shdr *shstr = (Elf64_Shdr*)(head + ehdr->e_shoff + ehdr->e_shentsize * ehdr->e_shstrndx);
-    std::vector<Elf64_Shdr*> shdrs(ehdr->e_shnum);
-    printf("Section:\n");
-    printf("idx: name offset size\n");
-    for (int i = 0; i < ehdr->e_shnum; i++) {
-        shdrs[i] = (Elf64_Shdr*)(head + ehdr->e_shoff + ehdr->e_shentsize * i);
-        char* sh_name = (char*)(head + shstr->sh_offset + shdrs[i]->sh_name);
-        printf("%d: %s 0x%x 0x%x\n", i, sh_name, (int)shdrs[i]->sh_offset, (int)shdrs[i]->sh_size);
-    }
-    printf("\n\n");
-
-    printf("Segment:\n");
-    printf("idx: (type) include sections...\n");
-    for (int i = 0; i < ehdr->e_phnum; i++) {
-        Elf64_Phdr *phdr = (Elf64_Phdr*)(head + ehdr->e_phoff + ehdr->e_phentsize * i);
-        const char* type = (phdr->p_type == PT_LOAD) ? "PT_LOAD" : 
-                     (phdr->p_type == PT_DYNAMIC) ? "PT_DYNAMIC" :
-                     (phdr->p_type == PT_PHDR) ? "PT_PHDR" : "UNKNOWN";
-        //printf("%d: %s 0x%x 0x%x |", i, type, (int)phdr->p_offset, (int)phdr->p_filesz);
-        printf("%d: (%s) ", i, type);
-        for (int j = 0; j < ehdr->e_shnum; j++) {
-            Elf64_Shdr *shdr = shdrs[j];
-            int size = (shdr->sh_type != SHT_NOBITS) ? shdr->sh_size : 0;
-            if (shdr->sh_offset < phdr->p_offset) continue;
-            if (shdr->sh_offset + size > phdr->p_offset + phdr->p_filesz) continue;
-            printf("%s ", (char*)(head + shstr->sh_offset + shdr->sh_name));
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-
-    printf("Symbols:\n");
-    for (int i = 0; i < ehdr->e_shnum; i++) {
-        Elf64_Shdr* shdr = shdrs[i];
-        if (shdr->sh_type != SHT_DYNSYM && shdr->sh_type != SHT_SYMTAB) continue;
-        char* sh_name = (char*)(head + shstr->sh_offset + shdr->sh_name);
-        printf("Section name: %s\n", sh_name);
-        printf("sh_link: %d\n", shdr->sh_link);
-        for (unsigned int j = 0; j < shdr->sh_size / shdr->sh_entsize; j++) {
-            Elf64_Sym* sym = (Elf64_Sym*)(head + shdr->sh_offset + shdr->sh_entsize * j);
-            const char* name = (const char*)(head + shdrs[shdr->sh_link]->sh_offset + sym->st_name);
-            if (!sym->st_name) continue;
-            printf("[%d] %s\n", j, name);
-        }
-        printf("\n");
-    }
-    printf("\n\n");
-
-    printf("Relocation table:\n");
-    for (int i = 0; i < ehdr->e_shnum; i++) {
-        Elf64_Shdr* shdr = shdrs[i];
-        if (shdr->sh_type != SHT_REL && shdr->sh_type != SHT_RELA) continue;
-        char* sh_name = (char*)(head + shstr->sh_offset + shdr->sh_name);
-        printf("sh_name: %s\n", sh_name);
-        for (unsigned int j = 0; j < shdr->sh_size / shdr->sh_entsize; j++) {
-            int sym_offset = 0;
-            if (shdr->sh_type == SHT_REL) {
-                Elf64_Rel* rel = (Elf64_Rel*)(head + shdr->sh_offset + shdr->sh_entsize * j);
-                sym_offset = ELF64_R_SYM(rel->r_info);
-            } else if (shdr->sh_type == SHT_RELA) {
-                Elf64_Rela* rela = (Elf64_Rela*)(head + shdr->sh_offset + shdr->sh_entsize * j);
-                sym_offset = ELF64_R_SYM(rela->r_info);
-            }
-            Elf64_Shdr* symtab = shdrs[shdr->sh_link];
-            Elf64_Sym* sym = (Elf64_Sym*)(head + symtab->sh_offset + symtab->sh_entsize * sym_offset);
-            if (!sym->st_name) continue;
-            const char* name = (const char*)(head + shdrs[symtab->sh_link]->sh_offset + sym->st_name);
-            printf("[%d]: %s\n", j, name);
-        }
-        printf("\n");
-    }
-    printf("\n\n");
+template<typename T>
+T round_up(T n, T m) {
+    return (n + (m-1)) & ~(m-1);
 }
 
-void load_files(char* head) {
+template<typename T>
+T round_down(T n, T m) {
+    return n & ~(m-1);
+}
+
+char* get_interp_name(char* head) {
+    Elf64_Ehdr *ehdr = (Elf64_Ehdr*)head;
+    for (int i = 0; i < ehdr->e_phnum; i++) {
+        Elf64_Phdr* phdr = (Elf64_Phdr*)(head + ehdr->e_phoff + ehdr->e_phentsize * i);
+        if (phdr->p_type == PT_INTERP) {
+            char* name = head + phdr->p_offset;
+            return name;
+        }
+    }
+    return nullptr;
+}
+
+char* load_files(char* head) {
     Elf64_Ehdr *ehdr = (Elf64_Ehdr*)head;
     if (!IS_ELF(*ehdr)) {
-        fprintf(stderr, "This is not ELF file.");
-        return;
+        fprintf(stderr, "This is not ELF file.\n");
+        return nullptr;
     }
+
+    //if (ehdr->e_type != ET_DYN) {
+    //    fprintf(stderr, "only support PIE files.\n");
+    //    return nullptr;
+    //}
+
+    uint64_t base_addr = -1;
+    size_t top_addr = 0;
+    // analyze required map size
+    for (int i = 0; i < ehdr->e_phnum; i++) {
+        Elf64_Phdr *phdr = (Elf64_Phdr*)(head + ehdr->e_phoff + ehdr->e_phentsize * i);
+        if (phdr->p_type != PT_LOAD) continue;
+        uint64_t vaddr = static_cast<uint64_t>(phdr->p_vaddr);
+        size_t msize = phdr->p_memsz;
+        if (base_addr > vaddr) base_addr = vaddr;
+        if (top_addr < vaddr + msize) top_addr = vaddr + msize;
+    }
+    uint64_t base_addr_align = round_down(base_addr, PAGE_SIZE);
+    uint64_t top_addr_align = round_up(top_addr, PAGE_SIZE);
+    //uint64_t base_offset = base_addr - base_addr_align;
+
+    // TODO: fix prot (use mprotect for each ph?)
+    size_t map_size = top_addr_align - base_addr_align;
+    char* map_head = (char*)mmap(&base_addr_align, map_size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE|MAP_ANON, -1, 0);
+    if (map_head == MAP_FAILED) {
+        perror("mmap");
+        return nullptr;
+    }
+    printf("Map head addr: 0x%lx, size: 0x%lx\n", (uint64_t)map_head, map_size);
 
     for (int i = 0; i < ehdr->e_phnum; i++) {
         Elf64_Phdr *phdr = (Elf64_Phdr*)(head + ehdr->e_phoff + ehdr->e_phentsize * i);
-        if (phdr->p_type == PT_LOAD) {
-            memcpy((char*)phdr->p_vaddr, head + phdr->p_offset, phdr->p_filesz);
-            printf("Segment %d loaded\n", i);
-        }
+        if (phdr->p_type != PT_LOAD) continue;
+        printf("load Segment %d\n", i);
+        uint64_t vaddr = static_cast<uint64_t>(phdr->p_vaddr);
+        size_t fsize = phdr->p_filesz;
+        printf("v_addr = 0x%lx, filesz = 0x%lx\n", vaddr, fsize);
+        // TODO: support prot with mprotect
+        int prot = 0;
+        if (phdr->p_flags & PROT_READ) prot |= PROT_READ;
+        if (phdr->p_flags & PROT_WRITE) prot |= PROT_WRITE;
+        if (phdr->p_flags & PROT_EXEC) prot |= PROT_EXEC;
+        memcpy(map_head + (vaddr - base_addr), head + phdr->p_offset, fsize);
+        printf("copy to 0x%lx\n", (uint64_t)(map_head + (vaddr - base_addr)));
     }
+
+    char* entry = map_head + (ehdr->e_entry - base_addr);
+    return entry;
 }
 
 int main(int argc, char* argv[]) {
@@ -126,10 +110,41 @@ int main(int argc, char* argv[]) {
     fstat(fd, &sb);
     char* head = (char*)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
-    load_files(head);
+    char* entry = load_files(head);
+    if (entry == nullptr) {
+        return -1;
+    }
+    printf("%s Entry point: 0x%lx\n", argv[1], (uint64_t)entry);
+
+    char* interp = get_interp_name(head);
+    char* interp_entry = nullptr;
+    if (interp != nullptr) {
+        printf("find interpreter %s.\n", interp);
+
+        int fd = open(interp, O_RDONLY);
+        if (fd < 0) {
+            fprintf(stderr, "cannot open %s\n", interp);
+            return -1;
+        }
+        struct stat sb;
+        fstat(fd, &sb);
+        char* elf_ld_head = (char*)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0);
+        char* interp_entry = load_files(elf_ld_head);
+        if (interp_entry == nullptr) {
+            return -1;
+        }
+        printf("%s Entry point: 0x%lx\n", interp, (uint64_t)entry);
+    }
 
     munmap(head, sb.st_size);
     close(fd);
+
+    //TODO: stack, arguments, aux vec
+
+    using ep = int(*)(int, char**, char**);
+    ep func = (interp_entry != nullptr) ? (ep)interp_entry : (ep)entry;
+    char* env[1] = {nullptr};
+    func(argc, argv, env);
 
     return 0;
 }
