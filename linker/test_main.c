f3bd40169541959ebc65b8780cf7ef8c6b9f082d
Ryosuke Kuramochi <kura19971106@gmail.com>
2023-01-26 00:56:03 +0900

    add uncommitted code

---
+++ linker/test_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sample_main();
+
+int main(int argc, char **argv, char **envp) {
+    //printf("argc = %d\n", argc);
+    //for (int i = 0; i < argc; i++) {
+    //    printf("%s ", argv[i]);
+    //}
+    //printf("\n");
+
+    sample_main();
+    return 0;
+}
